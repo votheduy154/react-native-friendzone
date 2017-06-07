a8cbe1d9ef86fd8919643d8be2c08dbf43b9ad00
DuyNguyen <duynguyen@DuyNguyens-iMac.local>
2017-06-08 04:48:42 +0700

    no message

---
+++ src/index.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class RNFriendZone extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Native!
+        </Text>
+        <Icon name="facebook" backgroundColor="#3b5998" >
+          Login with Facebook
+        </Icon>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+
+//AppRegistry.registerComponent('RNFriendZone', () => RNFriendZone);
